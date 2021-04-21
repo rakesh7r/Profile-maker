b1f801e5a981a731b42f7a97bd35fa11ee9cdde4
rakesh2002 <rakeshgandla200@gmail.com>
2021-04-21 13:11:13 +0530

    Profile-image modified with m-ui avatar

--- client/src/Main/profile.js
+++ client/src/Main/profile.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react"
+import Avatar from "@material-ui/core/avatar"
 import "./profile.css"
+
 class Profile extends Component {
     render() {
         return (
             <div className="profile-outer-container">
                 <div className="profile-left-container">
-                    <img
+                    {/* <img
                         src="lock_w (2).jpg"
                         alt="profile"
                         className="profile-img"
-                    />
+                    /> */}
+                    <Avatar
+                        alt="Rakesh"
+                        src="lock_w (2).jpg"
+                        style={{
+                            width: "250px",
+                            height: "250px",
+                            fontSize: "80px",
+                        }}
+                    ></Avatar>
                     <div className="profile-left-user-details">
                         <h3 className="profile-name">Rakesh Gandla</h3>
                         <span className="profile-username">@rakesh7r</span>
